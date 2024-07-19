1a0f543ab4b28af14bce125950937b7242d851de
tichys <tichman27@gmail.com>
2024-07-19 20:40:19 +0200

    Revert "Major codebase improvements + widescreen" (#1771)
    Revert "Major codebase improvements + widescreen (#1745)"
    
    This reverts commit 730915502da72834b49dc898d7430c2db92a4254.

--- tools/build/build.js
+++ tools/build/build.js
@@ -34,20 +34,11 @@ export const PortParameter = new Juke.Parameter({
   alias: "p",
 });
 
-export const DmVersionParameter = new Juke.Parameter({
-  type: 'string',
-});
-
 export const CiParameter = new Juke.Parameter({ type: 'boolean' });
 
 export const WarningParameter = new Juke.Parameter({
   type: "string[]",
   alias: "W",
-});
-
-export const NoWarningParameter = new Juke.Parameter({
-  type: 'string[]',
-  alias: 'I',
 });
 
 export const DmMapsIncludeTarget = new Juke.Target({
@@ -68,17 +59,19 @@ export const DmMapsIncludeTarget = new Juke.Target({
 });
 
 export const DmTarget = new Juke.Target({
-  parameters: [DefineParameter, DmVersionParameter, WarningParameter, NoWarningParameter],
+  parameters: [DefineParameter],
   dependsOn: ({ get }) => [
     get(DefineParameter).includes("ALL_MAPS") && DmMapsIncludeTarget,
   ],
   inputs: [
     "maps/**",
     "code/**",
+    'goon/**',
     "html/**",
     "icons/**",
     "interface/**",
     "mod_celadon/**",
+    'packs/**',
     `${DME_NAME}.dme`,
   ],
   outputs: [`${DME_NAME}.dmb`, `${DME_NAME}.rsc`],
@@ -86,8 +79,6 @@ export const DmTarget = new Juke.Target({
     await DreamMaker(`${DME_NAME}.dme`, {
       defines: ['CBT', ...get(DefineParameter)],
       warningsAsErrors: get(WarningParameter).includes('error'),
-      ignoreWarningCodes: get(NoWarningParameter),
-      namedDmVersion: get(DmVersionParameter),
     });
   },
 });
